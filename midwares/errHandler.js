f2bfd42e601389f7b1453a66677f937a5b511e36
dodytrifa <dody.trifa@gmail.com>
2021-02-12 15:51:21 +0700

    update server 1

--- midwares/errHandler.js
+++ midwares/errHandler.js
@@ -1,6 +1,4 @@
 module.exports = (err, req, res, next) => {
-    //log dulu
-    // console.log(err.msg);
 
     if (err.name == "SequelizeValidationError") {
         const errors = err.errors.map(el => el.message)
@@ -16,7 +14,4 @@ module.exports = (err, req, res, next) => {
     if (err.name == "JsonWebTokenError") {
         res.status(401).json(err.message)
     }
-    // else if (err.name == 'customError') {
-    //     res.status(err.status).json({ message: err.msg })
-    // }
 }
