ac201068f3710bf0aee50bc73c1c7e35151e8145
dodytrifa <dody.trifa@gmail.com>
2021-02-25 22:13:01 +0700

    redeploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //     require('dotenv').config()
 // }
 // versi localhost
-require('dotenv').config()
+// require('dotenv').config()
 
 const express = require('express');
 const port = process.env.PORT || 3000
